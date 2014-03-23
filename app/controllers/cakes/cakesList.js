deebc4d5e143139ca33a75fae25cd0cdf69d5cf5
Alexey Chulochnikov <chulochnikov@gmail.com>
2014-03-24 01:38:27 +0200

    - pull to refresh added to the cakes list

--- app/controllers/cakes/cakesList.js
+++ app/controllers/cakes/cakesList.js
@@ -1,16 +1,50 @@
 var args = arguments[0] || {};
 var Parse = require("TiParse")();
+var service = require('service');
 var cakesModel = Alloy.Collections.cakesModel;
 cakesModel.fetch();
 
 function doSelectItem(e) {
-	Ti.API.info('item clicked: ' + JSON.stringify(e));
-	var item = e.section.getItemAt(e.itemIndex);
-	Ti.API.info('item: ' + JSON.stringify(item));
+
 }
 
 function dataTransformFunction(model) {
 	var transform = model.toJSON();
 	transform.item = '[' + transform.item + ']';
 	return transform;
+}
+
+function doPull () {
+	$.pullLabel.textid = "ReleaseToRefreshTitile";
+}
+
+function doPullEnd () {
+	$.pullLabel.textid = "PullToRefreshTitile";
+
+	service.getAllContent({
+		success: function() {
+
+		},
+		error: function() {
+
+		}
+	});
+}
+
+function doItemClick (e) {
+	var item = e.section.getItemAt(e.itemIndex);
+
+	var ctrl = Alloy.createController("cakes/cakeDetails", {
+		name: item.cakeName.text,
+		shortDesc: item.cakeShortDesc.text,
+		picUrl: item.cakeImage.image,
+		desc: item.cakeDesc.cakeDesc,
+
+		price: item.cakePrice.cakePrice
+	});
+
+	var wnd = ctrl.getView();
+
+	Alloy.Globals.navWnd = $.cakesList;
+	Alloy.Globals.navWnd.openWindow(wnd);
 }
